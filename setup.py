f0b5d807caabf09bb633576235b394d6c17dbecf
vinylrishi <rsharma10598@gmail.com>
2023-03-28 19:10:44 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mod9',
+    version='1.0.0',
+    url='https://github.com/vinylrishi/DS5100-Mod9-Exercise',
+    author='Rishi Sharma',
+    author_email='bws5dk@virginia.edu',
+    description='Description of my package',
+    packages=find_packages('demo')    
+)
